758c4fb23f81beeb771b64000582ce49fafc1263
Pasindu Promod <dev@ninehermits.com>
2024-07-26 10:31:14 +0530

    template

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "DefaultCompany-Plank Pushers-0.1";
+const contentToCache = [
+    "Build/build.loader.js",
+    "Build/820c80cc3485b7af1bb5807e5ea2f8a5.js.gz",
+    "Build/105fbb7763362d526c28e70ee597cfed.data.gz",
+    "Build/a9b138c5388e7004064bd4c3c9546464.wasm.gz",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
