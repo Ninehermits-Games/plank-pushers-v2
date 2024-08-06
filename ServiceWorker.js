4d22e1ca248e695fbad4064d738ea2541d13d991
Pasindu Promod <dev@ninehermits.com>
2024-08-06 17:33:26 +0530

    lord help us

---
+++ ServiceWorker.js
@@ -0,0 +1,49 @@
+const cacheName = "DefaultCompany-Plank Pushers-0.6";
+const contentToCache = [
+    "Build/build.loader.js",
+    "Build/30fad2ef5e2678905ddebba07fd2f999.js",
+    "Build/f9d0783a437021a37e3a769484a56fb4.data",
+    "Build/95d12a16127a965330b096d66bdfafe8.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+
+	e.waitUntil(
+		caches.keys().then((cacheNames) => {
+		  return Promise.all(
+			cacheNames
+			  .filter((name) => {
+				return name !== cacheName;
+			  })
+			  .map((name) => {
+				console.log("[Service Worker] Deleting old cache:", name);
+				return caches.delete(name);
+			  })
+		  );
+		})
+	  );
+  
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
