c725ba8dbbd29e53be5dcbd50b295b5e20c6b0b6
Pasindu Promod <dev@ninehermits.com>
2024-08-06 10:36:30 +0530

    cache test 5

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,8 +1,8 @@
-const cacheName = "DefaultCompany-Plank Pushers-0.1";
+const cacheName = "DefaultCompany-Plank Pushers-0.2";
 const contentToCache = [
     "Build/build.loader.js",
     "Build/30fad2ef5e2678905ddebba07fd2f999.js.unityweb",
-    "Build/b1e69413a21b2d7395786202880509ae.data.unityweb",
+    "Build/3cc3ce3b2e012a8d9716cd00a3f45656.data.unityweb",
     "Build/e9eedb033b9cfcd6107a376c2122c99e.wasm.unityweb",
     "TemplateData/style.css"
 
@@ -11,6 +11,22 @@ const contentToCache = [
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+
+e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => {
+            return name !== cacheName;
+          })
+          .map((name) => {
+            console.log("[Service Worker] Deleting old cache:", name);
+            return caches.delete(name);
+          })
+      );
+    })
+  );
+  
     e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
