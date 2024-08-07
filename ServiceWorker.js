3bfda0c972098146951e2ce4a6b13b84c5ed93a2
Pasindu Promod <dev@ninehermits.com>
2024-08-07 10:58:04 +0530

    service worker fixed

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,78 +1,33 @@
-const cacheName = "DefaultCompany-Plank Pushers-0.7";
+const cacheName = "DefaultCompany-Plank Pushers-0.8";
 const contentToCache = [
-  "Build/build.loader.js",
-  "Build/8cb808cf1d5d11a6e827a997e4b5729b.js",
-  "Build/485def4b013e061c871b1bc47d8434a4.data",
-  "Build/61f0ecb6371871edcb7d2ce2dc77f24d.wasm",
-  "TemplateData/style.css",
+    "Build/build.loader.js",
+    "Build/ef16dc947541906a9e05874f3f38ef8c.js",
+    "Build/33dcaa1b46dac61063fb704af07d30bd.data",
+    "Build/2a8d47682661cdeec822b21cc3e7a8c5.wasm",
+    "TemplateData/style.css"
+
 ];
 
-self.addEventListener("install", function (e) {
-  console.log("[Service Worker] Install");
-
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => {
-            return name !== cacheName;
-          })
-          .map((name) => {
-            console.log("[Service Worker] Deleting old cache:", name);
-            return caches.delete(name);
-          })
-      );
-    })
-  );
-
-  e.waitUntil(
-    (async function () {
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
-      console.log("[Service Worker] Caching all: app shell and content");
+      console.log('[Service Worker] Caching all: app shell and content');
       await cache.addAll(contentToCache);
-    })()
-  );
+    })());
 });
 
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    (async function () {
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
       let response = await caches.match(e.request);
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) {
-        return response;
-      }
+      if (response) { return response; }
 
       response = await fetch(e.request);
       const cache = await caches.open(cacheName);
       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
-    })()
-  );
+    })());
 });
-
-// self.addEventListener("install", (event) => {
-//   // Skip waiting to activate immediately
-//   self.skipWaiting();
-// });
-
-// self.addEventListener("activate", (event) => {
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       return Promise.all(
-//         cacheNames.map((cacheName) => {
-//           return caches.delete(cacheName);
-//         })
-//       );
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
