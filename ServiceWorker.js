4bb89ec8aba1e50690cc790da2fa8a676321fe8b
Pasindu Promod <dev@ninehermits.com>
2024-08-09 22:16:08 +0530

    this fucking works

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "DefaultCompany-Plank Pushers-0.9";
-const contentToCache = [
-    "Build/build.loader.js",
-    "Build/7469918ec7a206f485dcee173a5b1bff.js",
-    "Build/f70ef6a1137cafbcb97b12cb86d77e63.data",
-    "Build/90c00e1afa7577f70ee85ba1341e9c09.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
